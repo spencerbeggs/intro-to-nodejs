a42e77f791016fab41886cd133145055b422d1d9
2741ded5a1d43f3a7cf249073689a97bd84bb92b <spencerbeggs@savvywebstrategy.com>
2014-07-26 10:18:03 -0400

    update

---
+++ process/index.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {
+		"Content-Type": "text/plain"
+	});
+	res.end(process.uptime().toString());
+}).listen(3000, "127.0.0.1");
+console.log("Server running at http://127.0.0.1:3000");
+
+server.on("connection", function() {
+	console.log("client connected!");
+});

272a6f7404b275196a037ed979bca585dfcbf4d1
C. Spencer Beggs <spencer@beg.gs>
2015-04-12 10:52:54 -0400

    bonus explanation in fs exercise

--- demos/filesystem/exercise/answer.js
+++ demos/filesystem/exercise/answer.js
@@ -1,16 +1,5 @@
-// Challenge: Write a program that reads the file sonnets.tx
-// in this directory. The file contains Shakspeare's 154 sonnets
-// seperated by the characters ---. Your program should seperate the
-// the sonnets into individual files and save them by number in the
-// output directory.
-
-// Use the Filesystem module: https://nodejs.org/api/fs.html
-
-// HINT: In JS you can reference a linebreak with "\t"
-// so "some text\t" is the line "some text" followed by a linebreak
-
-// get a reference to the filesystem module
 var fs = require("fs");
+var toRoman = require("roman-numerals").toRoman;
 
 // read the text file
 fs.readFile(__dirname + "/sonnets.txt", {
@@ -21,7 +10,7 @@ fs.readFile(__dirname + "/sonnets.txt", {
 	// loop over the array
 	for (var i = 0; i < sonnets.length; i++) {
 		//save each sonnet as its own file
-		fs.writeFile(__dirname + "/output/" + i, sonnets[i], function() {
+		fs.writeFile(__dirname + "/output/" + toRoman(i) + ".txt", sonnets[i], function() {
 			console.log("file saved");
 		});
 	}

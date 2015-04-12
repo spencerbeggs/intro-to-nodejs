497c1f384b1073f1a41495118694a6cb1b24fb04
C. Spencer Beggs <spencer@beg.gs>
2015-04-12 11:23:55 -0400

    update paths

--- demos/filesystem/read.js
+++ demos/filesystem/read.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-fs.readFile("./cat.txt", {
+fs.readFile(__dirname + "/cat.txt", {
 	encoding: "utf-8"
 }, function(err, data) {
 	if (err) {
--- demos/filesystem/write.js
+++ demos/filesystem/write.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-fs.writeFile("./dog.txt", "woof!", {
+fs.writeFile(__dirname + "/dog.txt", "woof!", {
 	encoding: "utf-8"
 }, function(err) {
 	if (err) {
